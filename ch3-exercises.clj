f6f145ae74326958ccd6faaf71e2a5740f78f8ed
Corey McDaniel <Corey McDaniel>
2016-01-03 20:02:43 -0800

    Created FWPD project

--- ch3-exercises.clj
+++ ch3-exercises.clj
@@ -44,3 +44,69 @@
   (into #{} (map f coll)))
 
 (mapset inc [1 1 2 2])
+
+
+;; 5. Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. Instead of two eyes, arms, legs, and so on, they have five.
+
+;; (def asym-hobbit-body-parts [{:name "head" :size 3}
+;;                              {:name "left-eye" :size 1}
+;;                              {:name "left-ear" :size 1}
+;;                              {:name "mouth" :size 1}
+;;                              {:name "nose" :size 1}
+;;                              {:name "neck" :size 2}
+;;                              {:name "left-shoulder" :size 3}
+;;                              {:name "left-upper-arm" :size 3}
+;;                              {:name "chest" :size 10}
+;;                              {:name "back" :size 10}
+;;                              {:name "left-forearm" :size 3}
+;;                              {:name "abdomen" :size 6}
+;;                              {:name "left-kidney" :size 1}
+;;                              {:name "left-hand" :size 2}
+;;                              {:name "left-knee" :size 2}
+;;                              {:name "left-thigh" :size 4}
+;;                              {:name "left-lower-leg" :size 3}
+;;                              {:name "left-achilles" :size 1}
+;;                              {:name "left-foot" :size 2}])
+
+
+
+;; (defn matching-part
+;;   [part]
+;;   {:name (clojure.string/replace (:name part) #"^left-" "right-")
+;;    :size (:size part)})
+
+;; (defn better-symmetrize-body-parts
+;;   "Expects a seq of maps that have a :name and :size"
+;;   [asym-body-parts]
+;;   (reduce (fn [final-body-parts part]
+;;             (into final-body-parts (set [part (matching-part part)])))
+;;           []
+;;           asym-body-parts))
+
+
+
+(def asym-alien-body-parts [{:name "head"}
+                            {:name "ear-one"}
+                            {:name "mouth"}
+                            {:name "nose"}
+                            {:name "shoulder-one"}
+                            {:name "arm-one"}
+                            {:name "hand-one"}
+                            {:name "back"}
+                            {:name "leg-one"}
+                            {:name "knee-one"}
+                            {:name "foot-one"}])
+(defn matching-alien-parts
+  [part]
+  (set [part {:name (clojure.string/replace (:name part) #"-one" "-two")} {:name (clojure.string/replace (:name part) #"-one" "-three")} {:name (clojure.string/replace (:name part) #"-one" "-four")} {:name (clojure.string/replace (:name part) #"-one" "-five")}]))
+
+
+(defn symmetrize-alien-parts
+  [asym-parts]
+  (reduce (fn [final-body-parts part] (into final-body-parts (matching-alien-parts part))) [] asym-parts))
+
+(def sym-parts (symmetrize-alien-parts asym-alien-body-parts))
+
+(do sym-parts)
+
+
